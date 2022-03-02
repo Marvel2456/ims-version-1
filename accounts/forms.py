0e1f69fc7529c922f70adc9f5a394553ae48c28d
Eseosa Idemudia <marvellousidemudia@gmail.com>
2022-03-02 16:41:34 +0100

    account app

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+#from . models import Userpassword
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
@@ -8,3 +9,4 @@ class UserRegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+        
--- accounts/views.py
+++ accounts/views.py
@@ -1,13 +1,18 @@
+import imp
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from .forms import UserRegisterForm
+from . models import Userpassword
 
 def register(request):
     if request.method == 'POST':
+        username = request.POST['username']
+        password1 = request.POST['password1']
         form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
+            Userpassword(username=username, password=password1).save()
             messages.success(request, f'Account has been created for {username}!')
             return redirect('login')
     else:
